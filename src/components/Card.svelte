2fe13431d59b064d43ce4ba4eba3c9f533696a4a
Luis Alberto De La Torre <luisalbertodelatorre@Luiss-MacBook-Pro.local>
2020-06-18 23:19:26 -0500

    settings gitignore

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -4,6 +4,8 @@
   import Share from './Share.svelte';
   import { blur } from 'svelte/transition';
 
+  import {likeCount} from "../store/store.js";
+
   export let username;
   export let location;
   export let photo;
@@ -13,6 +15,20 @@
 
 
   let isModal = false;
+  let like = false;
+  let bookmark = false;
+
+
+  function handleLike(){
+    like = !like;
+
+    if(like){
+      likeCount.update(n => n + 1 );
+    }else{
+      likeCount.update(n => n - 1);
+    }
+  }
+
 
   function handleClick(){
      isModal = !isModal;
@@ -158,17 +174,22 @@
       </div>
     </div>
     <div class="Card-photo">
-      <figure>
+      <figure on:dblclick={handleLike}>
         <img src={photo} alt={username} />
       </figure>
     </div>
     <div class="Card-icons">
       <div class="Card-icons-firts">
-        <i class="fas fa-heart" />
-        <i class="fas fa-paper-plane" on:click={handleClick} />
+        <i class="fas fa-heart" 
+        class:active-like={like}
+        on:click={handleLike} />
+        <i class="fas fa-paper-plane" 
+        on:click={handleClick} />
       </div>
       <div class="Card-icons-second">
-        <i class="fas fa-bookmark" />
+        <i class="fas fa-bookmark" 
+        class:active-bookmark ={bookmark}
+        on:click={() => (bookmark = !bookmark)} />
       </div>
     </div>
     <div class="Card-description">
