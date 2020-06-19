e07c71a6e1ca0fc24ac81f015e3b1ee1bbb037d6
Luis Alberto De La Torre <luisalbertodelatorre@Luiss-MacBook-Pro.local>
2020-06-18 19:07:14 -0500

    components Modal and Share

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -1,5 +1,8 @@
 <script>
   import Comments from "./Comments.svelte";
+  import Modal from "./Modal.svelte";
+  import Share from './Share.svelte';
+  import { blur } from 'svelte/transition';
 
   export let username;
   export let location;
@@ -7,6 +10,14 @@
   export let postComment;
   export let comments;
   export let avatar;
+
+
+  let isModal = false;
+
+  function handleClick(){
+     isModal = !isModal;
+  }
+
 </script>
 
 <style>
@@ -123,6 +134,16 @@
 </style>
 
 <div class="Card">
+    {#if isModal}
+     <div transition:blur>
+       <Modal>
+          <Share on:click={handleClick}></Share>
+       </Modal>
+        
+     </div>
+    {/if}
+
+
   <div class="Card-container">
     <div class="Card-Header">
       <div class="Card-user">
@@ -144,7 +165,7 @@
     <div class="Card-icons">
       <div class="Card-icons-firts">
         <i class="fas fa-heart" />
-        <i class="fas fa-paper-plane" />
+        <i class="fas fa-paper-plane" on:click={handleClick} />
       </div>
       <div class="Card-icons-second">
         <i class="fas fa-bookmark" />
